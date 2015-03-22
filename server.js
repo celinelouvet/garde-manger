f78321d5562f8ddbe671265e1ea9f88b7771ba83
Celine Louvet <celine.louvet@gmail.com>
2015-03-22 23:50:35 +0100

    Recipes route

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var recipes = require('./routes/route_recipes');
 
 var app = express();
 
 // serve static file
 app.use(express.static('.'));
+
+// Routing
+app.get('/recipe', recipes);
 
 // Listen port 8080
 app.listen(8080);
